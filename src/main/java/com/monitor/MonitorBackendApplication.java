73c22f9d0cb63c3079723a0d67cd692bb3913929
TheStick <palka.artur@outlook.com>
2017-01-18 21:43:14 +0100

    mqtt as a service test

--- src/main/java/com/monitor/MonitorBackendApplication.java
+++ src/main/java/com/monitor/MonitorBackendApplication.java
@@ -3,7 +3,17 @@ package com.monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
+import javax.websocket.MessageHandler;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.monitor")
@@ -13,4 +23,41 @@ public class MonitorBackendApplication {
 		SpringApplication.run(MonitorBackendApplication.class, args);
 	}
 
+	@Bean
+	public MessageChannel mqttInputChannel() {
+		return new DirectChannel();
+	}
+
+
+	@Bean
+	public MessageProducer inbound() {
+		MqttPahoMessageDrivenChannelAdapter adapter =
+				new MqttPahoMessageDrivenChannelAdapter("tcp://test.mosquitto.org", "pahomqttpublish1",
+						"temp/random");
+
+		adapter.setConverter(new DefaultPahoMessageConverter());
+		adapter.setQos(1);
+		adapter.setOutputChannel(mqttInputChannel());
+		adapter.addTopic();
+		return adapter;
+	}
+
+
+	@Bean
+	@ServiceActivator(inputChannel = "mqttInputChannel")
+	public MessageHandler handler() {
+		return new MessageHandler() {
+
+
+			public void handleMessage(Message<?> message) throws org.springframework.messaging.MessagingException {
+				System.out.println(message.getPayload());
+
+
+
+
+			}
+
+		};
+
+	}
 }
