efadb2aad61bec62ce8420741494b3a09153f097
TheStick <palka.artur@outlook.com>
2017-02-12 00:13:40 +0100

    fix

--- src/main/java/com/monitor/websockets/WebSocketConfig.java
+++ src/main/java/com/monitor/websockets/WebSocketConfig.java
@@ -21,6 +21,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websockets");
+        registry.addEndpoint("/websockets").setAllowedOrigins("*").withSockJS();
     }
 }
